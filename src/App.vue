50dedf164c7b0b2b4d14e46ec471c18449378f13
EmDeVee <MarcDiederik007@gmail.com>
2024-01-19 13:56:30 +0000

    Added CTA Buttons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
+<script setup>
+    import Slider from "./components/Slider.vue";
+    import CtaButton from "./components/CtaButton.vue"
+</script>
+
 <template>
      <section class="flex justify-center items-center">
         <div id="frame" class="container mt-6 justify-center max-w-screen-md relative">
             <div class="absolute z-30">
                 <img src="./assets/DinoRosieKAd.png" alt="" class="rounded-3xl shadow-slate-900 shadow-2xl drop-shadow-2xl">
                 <div class="absolute bottom-0 mb-8 grid grid-cols-5 gap-4 w-full">
+                    <div></div>
+                    <CtaButton label="Subscribe" />
+                    <div></div>
+                    <CtaButton label="Visit my Store" />
+                    <div></div>
                 </div>
             </div>
             <Slider />
@@ -11,12 +21,3 @@
     </section>
 </template>
 
-<script>
-    import Slider from "./components/Slider.vue";
-    export default {
-        name: "app",
-        components: {
-            Slider
-        }
-    };
-</script>
---
+++ src/components/CtaButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['label']);
+</script>
+
+<template>
+    <button class="bg-purple-600 hover:bg-purple-950 text-white font-bold py-2 rounded-full">
+        {{ label }}
+    </button>
+</template>
+
