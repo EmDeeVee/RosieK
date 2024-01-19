8e124186d7d072057b139b66f27a9d8fd5030015
EmDeVee <MarcDiederik007@gmail.com>
2024-01-19 12:56:56 +0000

    Image Slider working

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+     <section class="flex justify-center items-center">
+        <div id="frame" class="container mt-6 justify-center max-w-screen-md relative">
+            <div class="absolute z-30">
+                <img src="./assets/DinoRosieKAd.png" alt="" class="rounded-3xl shadow-slate-900 shadow-2xl drop-shadow-2xl">
+                <div class="absolute bottom-0 mb-8 grid grid-cols-5 gap-4 w-full">
+                </div>
+            </div>
+            <Slider />
+        </div>
+    </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+    import Slider from "./components/Slider.vue";
+    export default {
+        name: "app",
+        components: {
+            Slider
+        }
+    };
+</script>
---
+++ src/components/Slider.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+      <transition-group name="fade" tag="div">
+        <div v-for="i in [currentIndex]" :key="i">
+          <img :src="currentImg" />
+        </div>
+      </transition-group>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Slider",
+    data() {
+      return {
+        images: [
+            "/BlubDinoMsk.png",
+            "/BookLoverMsk.png",
+            "/GilbertMsk.png",
+            "/HuggingRex-iesMsk.png",
+            "/BlubFishMsk.png",
+            "/PatTeraDactieMsk.png",
+            "/GhostyDinoMsk.png",
+            "/SquidMsk.png",
+            "/PandaInABoxMsk.png",
+            "/SleepyPuppyMsk.png",
+            "/GnomeShittingMsk.png",
+            "/LittleOctopusMsk.png",
+            "/RosieDinoLogoMsk.png",
+        ],
+        timer: null,
+        currentIndex: 0
+      };
+    },
+  
+    mounted: function() {
+      this.startSlide();
+    },
+  
+    methods: {
+      startSlide: function() {
+        this.timer = setInterval(this.next, 2000);
+      },
+  
+      next: function() {
+        this.currentIndex += 1;
+      },
+    },
+  
+    computed: {
+      currentImg: function() {
+        return this.images[Math.abs(this.currentIndex) % this.images.length];
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.9s ease;
+  overflow: hidden;
+  visibility: visible;
+  position: absolute;
+  width:100%;
+  opacity: 1;
+}
+
+.fade-enter,
+.fade-leave-to {
+  visibility: hidden;
+  width:100%;
+  opacity: 0;
+}
+</style>
