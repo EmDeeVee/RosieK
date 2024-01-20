4c1c297031d1d6bb4e5468d3df4e8e39d8b5728c
EmDeVee <MarcDiederik007@gmail.com>
2024-01-20 04:13:26 +0000

    placed form components in subscribe

--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -5,7 +5,23 @@
     <div class="absolute z-30">
         <img src="../assets/DinoRosieKSubscribe.png" alt="" class="rounded-3xl shadow-slate-900 shadow-2xl drop-shadow-2xl">
         <div class="absolute bottom-0 mb-8 grid grid-cols-5 gap-4 w-full">
-            <!-- buttons-->
+            <form>
+                <p>Name</p>
+                <input type="text" v-model="name" />
+                <p>Email</p>
+                <input type="text" v-model="emal" />
+                <div>
+                    <div>Gender:</div>
+                    <input name="gendersel" type="radio" id="male" value="1" v-model="gender" />
+                    <label for="male">Male</label>
+                    <br>
+                    <input name="gendersel" type="radio" id="female" value="2" v-model="gender" />
+                    <label for="female">Female</label>
+                    <br>
+                    <input name="gendersel" type="radio" id="unknown" value="3" v-model="gender" checked />
+                    <label fo="unknown">Rather not Say</label>
+                </div>
+            </form>
         </div>
     </div>
 </template>
