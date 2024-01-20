5481775f0eb4efa0d37d69d322429c8a4d4c6bfa
EmDeVee <MarcDiederik007@gmail.com>
2024-01-20 00:50:45 +0000

    redesigned locatoin of buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,12 +6,9 @@
 <template>
     <div class="absolute z-30">
         <img src="../assets/DinoRosieKAd.png" alt="" class="rounded-3xl shadow-slate-900 shadow-2xl drop-shadow-2xl">
-        <div class="absolute bottom-0 mb-8 grid grid-cols-5 gap-4 w-full">
-            <div></div>
-            <CtaButton id='subscribe' label="Subscribe" dest="#/subscribe"/>
-            <div></div>
-            <CtaButton id='visit' label="Visit my Store" dest="#/question"/>
-            <div></div>
+        <div class="absolute bottom-0 mb-8 grid grid-cols-12 gap-2 w-full">
+            <CtaButton id='subscribe' label="Subscribe" dest="#/subscribe" class="col-start-2 col-span-4"/>
+            <CtaButton id='visit' label="Visit my Store" dest="#/question" class="col-start-8 col-span-4"/>
         </div>
     </div>
     <Slider />
